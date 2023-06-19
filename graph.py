5e218d5f905f8b6440874f9e38ab6642a3d74d3f
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-06-19 12:18:22 +0530

    fii

---
+++ graph.py
@@ -0,0 +1,138 @@
+""" import matplotlib.pyplot as plt
+
+plt.plot([3,4,5,6,2,7],[3,4,5,6,2,7] , 'o')
+plt.show()  """
+
+""" from matplotlib import pyplot as plt
+import numpy as np
+import math #needed for definition of pi
+x = np.arange(0, math.pi*2, 0.05)
+print(x)
+
+y = np.sin(x)
+plt.plot(x,y)
+plt.xlabel("angle")
+plt.ylabel("sine")
+plt.title('sine wave')
+plt.show()  """
+
+""" from numpy import *
+from pylab import *
+x = linspace(-3, 3, 30)
+print(x)
+y = x**2
+plot(x, y)
+show() """
+
+""" from pylab import *
+x = linspace(-3, 3, 30)
+y = x**2
+plot(x, y, 'r.')
+show() """
+
+""" from pylab import *
+x = linspace(-3, 3, 30)
+plot(x, sin(x))
+plot(x, cos(x), 'r-')
+plot(x, -sin(x), 'g--')
+show() """
+
+""" from matplotlib import pyplot as plt
+import numpy as np
+import math
+x = np.arange(0, math.pi*2, 0.05)
+y = np.sin(x)
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.plot(x,y)
+ax.set_title("sine wave")
+ax.set_xlabel('angle')
+ax.set_ylabel('sine')
+plt.show()  """
+
+""" import matplotlib.pyplot as plt
+y = [1, 4, 9, 16, 25,36,49, 64]
+x1 = [1, 16, 30, 42,55, 68, 77,88]
+x2 = [1,6,12,18,28, 40, 52, 65]
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+l1 = ax.plot(x1,y,'ys-') # solid line with yellow colour and square marker
+l2 = ax.plot(x2,y,'go--') # dash line with green colour and circle marker
+ax.legend(labels = ('tv', 'Smartphone'), loc = 'lower right') # legend placed at lower right
+ax.set_title("Advertisement effect on sales")
+ax.set_xlabel('medium')
+ax.set_ylabel('sales')
+plt.show()  """
+
+""" import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+langs = ['C', 'C++', 'Java', 'Python', 'PHP']
+students = [23,17,35,29,12]
+ax.bar(langs,students)
+plt.show() """
+
+import numpy as np
+import matplotlib.pyplot as plt
+N = 5
+menMeans = (20, 35, 30, 35, 27)
+womenMeans = (25, 32, 34, 20, 25)
+ind = np.arange(N) # the x locations for the groups
+width = 0.35
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.bar(ind, menMeans, width, color='r')
+ax.bar(ind, womenMeans, width,bottom=menMeans, color='b')
+ax.set_ylabel('Scores')
+ax.set_title('Scores by group and gender')
+ax.set_xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
+ax.set_yticks(np.arange(0, 81, 10))
+ax.legend(labels=['Men', 'Women'])
+plt.show() 
+
+""" from matplotlib import pyplot as plt
+import numpy as np
+fig,ax = plt.subplots(1,1)
+a = np.array([22,87,5,43,56,73,55,54,11,20,51,5,79,31,27])
+ax.hist(a, bins = [0,25,50,75,100])
+ax.set_title("histogram of result")
+ax.set_xticks([0,25,50,75,100])
+ax.set_xlabel('marks')
+ax.set_ylabel('no. of students')
+plt.show() """
+""" from matplotlib import pyplot as plt
+import numpy as np
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.axis('equal')
+langs = ['C', 'C++', 'Java', 'Python', 'PHP']
+students = [23,17,35,29,12]
+ax.pie(students, labels = langs,autopct='%1.2f%%')
+plt.show() """
+""" 
+import matplotlib.pyplot as plt
+girls_grades = [89, 90, 70, 89, 100, 80, 90, 100, 80, 34]
+boys_grades = [30, 29, 49, 48, 100, 48, 38, 45, 20, 30]
+grades_range = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+fig=plt.figure()
+ax=fig.add_axes([0,0,1,1])
+ax.scatter(grades_range, girls_grades, color='r')
+ax.scatter(grades_range, boys_grades, color='b')
+ax.set_xlabel('Grades Range')
+ax.set_ylabel('Grades Scored')
+ax.set_title('scatter plot')
+plt.show() """
+""" 
+import numpy as np
+import matplotlib.pyplot as plt
+xlist = np.linspace(-3.0, 3.0, 100)
+ylist = np.linspace(-3.0, 3.0, 100)
+X, Y = np.meshgrid(xlist, ylist)
+Z = np.sqrt(X**2 + Y**2)
+fig,ax=plt.subplots(1,1)
+cp = ax.contourf(X, Y, Z)
+fig.colorbar(cp) # Add a colorbar to a plot
+ax.set_title('Filled Contours Plot')
+#ax.set_xlabel('x (cm)')
+ax.set_ylabel('y (cm)')
+plt.show() """
